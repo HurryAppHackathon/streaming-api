9e7170eed865bc19c21bbd4a61f1cf94c1281beb
Leda <me@leda.dev>
2024-01-12 12:54:16 +0000

    feat: login & register routes

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,32 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\ForgotPasswordRequest;
-use App\Http\Requests\LoginRequest;
-use App\Http\Requests\RegisterUserRequest;
-use App\Http\Requests\ResetPasswordRequest;
-use App\Http\Requests\VerifyOtpRequest;
+use App\Http\Requests\UserRegisterRequest;
+use App\Http\Requests\UserLoginRequest;
 use App\Models\User;
-use App\OpenApi\RequestBodies\UserForgotPasswordRequestBody;
-use App\OpenApi\RequestBodies\UserLoginRequestBody;
-use App\OpenApi\RequestBodies\UserRegisterRequestBody;
-use App\OpenApi\RequestBodies\UserResetPasswordRequestBody;
-use App\OpenApi\RequestBodies\VerifyOtpRequestBody;
-use App\OpenApi\Responses\EmptyResponse;
-use App\OpenApi\Responses\ErrorMessageResponse;
-use App\OpenApi\Responses\ErrorValidationResponse;
-use App\OpenApi\Responses\GetCurrentUserResponse;
-use App\OpenApi\Responses\UnauthenticatedResponse;
-use App\OpenApi\Responses\UserLoginResponse;
-use App\OpenApi\Responses\UserRegisterResponse;
-use App\Rules\PhoneNumber;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
-use Vyuldashev\LaravelOpenApi\Attributes as OpenApi;
 
-#[OpenAPI\PathItem]
 class AuthController extends Controller
 {
     /**
@@ -45,24 +27,14 @@ class AuthController extends Controller
      * 
      * Registers a new user account from given data
      */
-    public function register(RegisterUserRequest $request)
+    public function register(UserRegisterRequest $request)
     {
         $validated = $request->validated();
 
-        // Check if user with phone_number exists and is verfied
-        $isPhoneAlreadyRegisteredAndVerified = User::where('phone_number', $validated['phone_number'])->whereNotNull('phone_number_verified_at')->exists();
-
-        if ($isPhoneAlreadyRegisteredAndVerified) {
-            return abort(Response::HTTP_BAD_REQUEST, __('auth.phone_already_registered'));
-        }
-
-        $user = User::updateOrCreate(
+        $user = User::create(
             [
-                'phone_number' => $validated['phone_number'],
-            ],
-            [
-                'full_name' => $validated['full_name'],
-                'phone_number' => $validated['phone_number'],
+                'username' => $validated['username'],
+                'email' => $validated['email'],
                 'password' => $validated['password'],
             ]
         );
@@ -77,15 +49,15 @@ class AuthController extends Controller
      * 
      * Logs in user with provided credentials
      */
-    public function login(LoginRequest $request)
+    public function login(UserLoginRequest $request)
     {
         $validated = $request->validated();
 
-        $user = User::where('phone_number', $validated['phone_number'])->first();
+        $user = User::where('username', $validated['username'])->first();
 
         if (!$user || !Hash::check($validated['password'], $user->password)) {
             throw ValidationException::withMessages([
-                'phone_number' => ['The provided credentials are incorrect.'],
+                'username' => ['The provided credentials are incorrect.'],
             ]);
         }
 
